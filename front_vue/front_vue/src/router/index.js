723d1e85149c1cb26364bdd6a116a16b0407d617
WatsonJay <446725026@qq.com>
2019-07-24 17:37:29 +0800

    update

--- front_vue/front_vue/src/router/index.js
+++ front_vue/front_vue/src/router/index.js
@@ -3,34 +3,35 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+//所有权限通用路由表
+//如首页和登录页和一些不用权限的公用页面
+export const constantRouterMap = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: resolve => require(['@/views/Login.vue'], resolve)
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: resolve => require(['@/views/Register.vue'], resolve)
+  },
+  {
+    path: '/',
+    name: 'Home',
+    redirect: '/dashboard',
+    component: resolve => require(['@/views/Home.vue'], resolve),
+    meta: { title: '基础平台' },
+    children: [{ path: 'dashboard', component: resolve => require(['@/views/NoFound.vue'], resolve) }]
+  },
+]
+export const asyncRouterMap = [
+  {
+    path:'*',
+    component : resolve => require(['@/views/NoFound.vue'], resolve)
+  },
+]
 export default new Router({
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      redirect: {
-        name: 'Home'
-      }
-    },
-    {
-      path: '/',
-      name: 'Home',
-      component: resolve => require(['@/views/Home.vue'], resolve),
-      meta: { title: '基础平台' },
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: resolve => require(['@/views/Login.vue'], resolve)
-    },
-    {
-      path: '/register',
-      name: 'Register',
-      component: resolve => require(['@/views/Register.vue'], resolve)
-    },
-    {
-      path:'*',
-      component : resolve => require(['@/views/NoFound.vue'], resolve)
-    },
-  ]
+  routes: constantRouterMap
 })
